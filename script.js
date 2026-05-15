7d487a088488d53e873f6d4c685396a8cb48c896
sravangantla007 <gvsravan007@gmail.com>
2026-05-15 20:20:48 +0530

    first

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Add smooth scrolling for all anchor links
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            
+            const targetId = this.getAttribute('href');
+            if(targetId === '#') return;
+
+            const targetElement = document.querySelector(targetId);
+            
+            if (targetElement) {
+                targetElement.scrollIntoView({
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
+});
